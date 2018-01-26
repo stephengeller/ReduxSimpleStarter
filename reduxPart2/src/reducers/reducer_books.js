71fe99ac3a65d329c8b341960eccaddc95702516
Stephen Geller <stephen.geller@hotmail.com>
2018-01-26 17:18:59 +0000

    fixed git folder issues with part 2

---
+++ reduxPart2/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'Harry Potter: The Cursed Child' },
+        { title: 'Lord of the Rings: Return of The King' },
+        { title: 'Eragon' },
+        { title: 'Drive' }
+    ]
+}
